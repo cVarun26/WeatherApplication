91d96b273bf24284da8841942c0d9f687c5f4451
C Varun <cvarun561@gmail.com>
2025-06-29 00:34:08 +0530

    added gitignore

--- src/main/java/com/example/WeatherAPP/constant/Constants.java
+++ src/main/java/com/example/WeatherAPP/constant/Constants.java
@@ -2,7 +2,7 @@ package com.example.WeatherAPP.constant;
 
 public final class Constants {
     public static final String API_URL = "http://api.openweathermap.org/data/2.5/weather";
-    public static final String API_KEY = "6f12ae61b995ae19aa173a138291577c";
+    public static final String API_KEY = "f8816daff120224c66e4335c73d8d25f";
     public static final String errorMessage = "An unexpected error detected!";
     public static int breachCount = 2;
     public static String EMAIL = "johndoe260703@gmail.com";
